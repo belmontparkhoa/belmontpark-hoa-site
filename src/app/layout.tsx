f68a33018c856bf5bc17dcc5e6af50c0d99f9127
thomas-tomlinson <thomas_tomlinson@mac.com>
2026-01-29 15:24:36 -0700

    footer for mail and GitHub repo (#1)
    * added email and github footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "../components/Navbar"
+import Navbar from "../components/Navbar";
+import SocialIcon from "@/components/social-icons";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -27,9 +28,17 @@ export default function RootLayout({
 		<html lang="en">
 			<head>
 			</head>
-			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+			<body className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-svh`}>
                 <Navbar />
-                {children}
+                <main className="flex-grow">{children}</main>
+            <footer className="p-5">
+                <div className="flex flex-col items-center">
+                <div className="mb-3 flex space-x-4">
+                <SocialIcon href='mailto:bphoa.boise@gmail.com' kind="mail" />
+                <SocialIcon href='https://github.com/belmontparkhoa/belmontpark-hoa-site' kind="github" />
+                </div>
+                </div>
+            </footer>
             </body>
 		</html>
 	);
---
+++ src/components/social-icons/index.tsx
@@ -0,0 +1,41 @@
+import {
+  Mail,
+  Github,
+} from './icons'
+
+const components = {
+  mail: Mail,
+  github: Github,
+}
+
+type SocialIconProps = {
+  kind: keyof typeof components
+  href: string | undefined
+  size?: number
+}
+
+const SocialIcon = ({ kind, href, size = 8 }: SocialIconProps) => {
+  if (
+    !href ||
+    (kind === 'mail' && !/^mailto:[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(href))
+  )
+    return null
+
+  const SocialSvg = components[kind]
+
+  return (
+    <a
+      className="text-sm text-gray-500 transition hover:text-gray-600"
+      target="_blank"
+      rel="noopener noreferrer"
+      href={href}
+    >
+      <span className="sr-only">{kind}</span>
+      <SocialSvg
+        className={`hover:text-primary-500 dark:hover:text-primary-400 fill-current text-gray-700 dark:text-gray-200 size-6 `}
+      />
+    </a>
+  )
+}
+
+export default SocialIcon
